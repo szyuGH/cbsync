729d59e7468aa07f26a73edc2aa8f97d5d1802db
J.Heinen <j.heinen@eurokey.de>
2018-02-09 10:55:28 +0100

    added async data load

---
+++ CBSync/CBSync/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBSync
+{
+    public static class Extensions
+    {
+        public static void Sort<T, K>(this ObservableCollection<T> observable, Func<T, K> keySelector, IComparer<K> comp)
+        {
+            List<T> sorted = observable.OrderBy(keySelector, comp).ToList();
+            observable.Clear();
+            foreach (T s in sorted)
+            {
+                observable.Add(s);
+            }
+        }
+    }
+}
--- CBSync/CBSync/MainWindow.xaml.cs
+++ CBSync/CBSync/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,18 +27,82 @@ namespace CBSync
     {
         public ObservableCollection<NetworkHost> MyList { get; set; } = new ObservableCollection<NetworkHost>();
 
+        private IPNetwork network;
+
         public MainWindow()
         {
             InitializeComponent();
-            
-            MyList.Add(new NetworkHost()
+
+
+            Task.Run(() => LoadData());
+        }
+
+        private void Test()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
             {
-                IP = "123.123.123.123",
-                HostName = "Bum",
-                SyncState = "X",
+                MyList.Add(new NetworkHost()
+                {
+                    IP = "123.123.123.123",
+                    HostName = "Bum",
+                    SyncState = "X",
+                });
+            }));
+            
+        }
+
+        private async void LoadData()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces().Where(i => i.NetworkInterfaceType != NetworkInterfaceType.Loopback && i.OperationalStatus == OperationalStatus.Up))
+            {
+                UnicastIPAddressInformation ownIp = ni.GetIPProperties().UnicastAddresses
+                    .Where(i => i.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    .FirstOrDefault();
+
+                network = new IPNetwork(ownIp.Address, ownIp.IPv4Mask);
+
+                Application.Current.Dispatcher.Invoke(() => pb_LoadStatus.Maximum = network.HostCount);
+                foreach (IPAddress ip in network.IterateUsableIPs())
+                {
+                    await Task.Factory.StartNew(() => LoadTask(ip));
+                }
+            }
+        }
+
+        private async void LoadTask(IPAddress ip)
+        {
+            Ping ping = new Ping();
+            PingReply res = await ping.SendPingAsync(ip, 100);
+            if (res.Status == IPStatus.Success)
+            {
+                string hostname = "";
+                try
+                {
+                    IPHostEntry entry = await Dns.GetHostEntryAsync(ip);
+                    hostname = entry.HostName;
+                }
+                catch (Exception e)
+                {
+                    hostname = "";
+                }
+                Application.Current.Dispatcher.Invoke(() => MyList.Add(new NetworkHost()
+                {
+                    IP = ip.ToString(),
+                    HostName = hostname,
+                    SyncState = "X",
+                }));
+                
+            }
+            Application.Current.Dispatcher.Invoke(() => pb_LoadStatus.Value++);
+
+
+            Application.Current.Dispatcher.Invoke(() => {
+                if (pb_LoadStatus.Value >= pb_LoadStatus.Maximum)
+                {
+                    Application.Current.Dispatcher.Invoke(() => pb_LoadStatus.Value = 0);
+                }
             });
             
-
         }
 
         private void SyncButton_Click(object sender, RoutedEventArgs e)
@@ -54,6 +122,11 @@ namespace CBSync
             // TODO: Sync
         }
 
+        private void btn_Sort_Click(object sender, RoutedEventArgs e)
+        {
+            MyList.Sort(i => i.IP, new RowComp());
+        }
+
         public class NetworkHost
         {
             public string IP { get; set; }
@@ -61,6 +134,25 @@ namespace CBSync
             public string SyncState { get; set; }
         }
 
-        
+        public class RowComp : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                if (x is string && y is string)
+                {
+                    IPAddress ip1 = IPAddress.Parse(x);
+                    IPAddress ip2 = IPAddress.Parse(y);
+
+                    byte[] ipb1 = ip1.GetAddressBytes();
+                    byte[] ipb2 = ip2.GetAddressBytes();
+                    return (Math.Pow(ipb1[0], 4) + Math.Pow(ipb1[1], 3) + Math.Pow(ipb1[2], 2) + Math.Pow(ipb1[3], 1)).CompareTo(
+                        Math.Pow(ipb2[0], 4) + Math.Pow(ipb2[1], 3) + Math.Pow(ipb2[2], 2) + Math.Pow(ipb2[3], 1));
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
     }
 }
