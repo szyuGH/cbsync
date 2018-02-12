1620e71b5837051bf185a8cf945022279db7e024
J.Heinen <j.heinen@eurokey.de>
2018-02-12 14:36:07 +0100

    added owin self host with simple test controller

--- CBSync/CBSync/App.xaml.cs
+++ CBSync/CBSync/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System.Web.Http;
 using System.Windows;
 
 namespace CBSync
@@ -13,5 +11,29 @@ namespace CBSync
     /// </summary>
     public partial class App : Application
     {
+        public static string baseWebAPIAddress = "http://localhost:9000/";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            WebApp.Start<OWINWebAPIConfig>(url: baseWebAPIAddress);
+        }
+    }
+
+    public class OWINWebAPIConfig
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
     }
 }
---
+++ CBSync/CBSync/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CBSync
+{
+    public class TestController : ApiController
+    {
+        [HttpPost]
+        public string GetTest()
+        {
+            return "Hello World";
+        }
+    }
+}
